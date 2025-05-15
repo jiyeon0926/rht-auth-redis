498a7670410680b1ab58100d38bf713b35c281f2
jiyeon0926 <yeoni9094@naver.com>
2025-05-15 17:33:01 +0900

    feat: User Entity

---
+++ src/main/java/auth/demo/domain/user/entity/User.java
@@ -0,0 +1,37 @@
+package auth.demo.domain.user.entity;
+
+import auth.demo.global.common.entity.BaseEntity;
+import auth.demo.global.common.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 10)
+    private String name;
+
+    @Column(nullable = false, length = 10)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    public User(String email, String password, String name, String role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.role = UserRole.of(role);
+    }
+}
