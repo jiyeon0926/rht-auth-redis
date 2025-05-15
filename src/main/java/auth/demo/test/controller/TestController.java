90c6acd28b5c3232b7de5d5ef160c6c211ce99ed
jiyeon0926 <yeoni9094@naver.com>
2025-05-15 19:03:30 +0900

    test: TestController
    - 인증, 권한 확인용 컨트롤러

---
+++ src/main/java/auth/demo/test/controller/TestController.java
@@ -0,0 +1,20 @@
+package auth.demo.test.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/auth/check")
+    public ResponseEntity<String> authCheck() {
+        return new ResponseEntity<>("인증된 사용자입니다.", HttpStatus.OK);
+    }
+
+    @GetMapping("/admins/check")
+    public ResponseEntity<String> adminCheck() {
+        return new ResponseEntity<>("관리자만 접근할 수 있습니다.", HttpStatus.OK);
+    }
+}
