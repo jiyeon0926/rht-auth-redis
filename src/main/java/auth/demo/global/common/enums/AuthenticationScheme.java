a2870e1ac885ac1a621ad2d19aac0e7f03fbab4c
jiyeon0926 <yeoni9094@naver.com>
2025-05-15 18:23:01 +0900

    feat: AuthenticationScheme

---
+++ src/main/java/auth/demo/global/common/enums/AuthenticationScheme.java
@@ -0,0 +1,16 @@
+package auth.demo.global.common.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthenticationScheme {
+    BEARER("Bearer");
+
+    private final String name;
+
+    public static String generateType(AuthenticationScheme authenticationScheme) {
+        return authenticationScheme.getName() + " ";
+    }
+}
